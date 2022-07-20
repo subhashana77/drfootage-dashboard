d26ab79b9098c7243e280292820881742c5407d2
Dilshan Subhashana Rajapaksha <106365502+subhashana77@users.noreply.github.com>
2022-07-21 00:26:10 +0530

    fix some bugs in searchable box

--- src/component/Edit-remove-footage.jsx
+++ src/component/Edit-remove-footage.jsx
@@ -1,6 +1,35 @@
 import React, {useEffect, useRef, useState} from 'react';
 
-function EditRemoveFootage() {
+function EditRemoveFootage(props) {
+
+    // const defaultOptions = [];
+    // for (let i = 0; i < 10; i++) {
+    //     defaultOptions.push(`option ${i}`);
+    //     defaultOptions.push(`suggestion ${i}`);
+    //     defaultOptions.push(`advice ${i}`);
+    // }
+    //
+    // const [options, setOptions] = useState([]);
+    //
+    // const ulRef = useRef();
+    // const inputRef = useRef();
+    //
+    // useEffect(() => {
+    //     inputRef.current.addEventListener('click', (event) => {
+    //         event.stopPropagation();
+    //         ulRef.current.style.display = 'flex';
+    //         onSearchHandler(event);
+    //     })
+    //     document.addEventListener('click', (event) => {
+    //         ulRef.current.style.display = 'none';
+    //     });
+    // },[]);
+    //
+    // const onSearchHandler = (event) => {
+    //     setOptions(defaultOptions.filter(option => option.includes(event.target.value)));
+    // }
+
+
 
     const [allImageNameArr, setAllImageNamesArr] = useState([]);
 
@@ -20,16 +49,38 @@ function EditRemoveFootage() {
         inputRef.current.addEventListener('click', (event) => {
             event.stopPropagation();
             buttonRef.current.style.display = 'flex';
+            loadAllImagesName();
         })
         document.addEventListener('click', (event) => {
             buttonRef.current.style.display = 'none';
+            loadAllImagesName();
         });
     }, []);
 
+    const onSearchHandler = (event) => {
+        setAllImageNamesArr(allImageNameArr.filter(imageNames => (imageNames.footage_name).includes(event.target.value)));
+    }
+
 
     return (
+        // <div className="search-bar-dropdown">
+        //     <input id="search-bar" type="text" ref={inputRef} className="form-control" placeholder="Search" onChange={onSearchHandler}/>
+        //     <ul id="results" className="list-group" ref={ulRef}>
+        //         {
+        //             options.map((option, index) => (
+        //                 <button key={index} type="button" className="list-group-item list-group-item-dark" onClick={(e) =>{
+        //                     inputRef.current.value = option;
+        //                 }}>
+        //                     {option}
+        //                 </button>
+        //             ))
+        //         }
+        //     </ul>
+        //     <button className="btn btn-primary">Search</button>
+        // </div>
+
         <div className="search-bar-dropdown">
-            <input id="search-bar" ref={inputRef} type="text" className="form-control" placeholder="Search"/>
+            <input id="search-bar" type="text" ref={inputRef} className="form-control" placeholder="Search..." onChange={onSearchHandler}/>
             <ul id="results" className="list-group" ref={buttonRef}>
                 {
                     allImageNameArr.map((imageNames, index) => (

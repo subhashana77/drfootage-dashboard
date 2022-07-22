4402296074ccaba77ba2265489cf53dae3d3795f
Dilshan Subhashana Rajapaksha <106365502+subhashana77@users.noreply.github.com>
2022-07-22 13:00:35 +0530

    get the full name with unique id to varible to search the image by name

--- src/component/Edit-remove-footage.jsx
+++ src/component/Edit-remove-footage.jsx
@@ -1,35 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 
-function EditRemoveFootage(props) {
-
-    // const defaultOptions = [];
-    // for (let i = 0; i < 10; i++) {
-    //     defaultOptions.push(`option ${i}`);
-    //     defaultOptions.push(`suggestion ${i}`);
-    //     defaultOptions.push(`advice ${i}`);
-    // }
-    //
-    // const [options, setOptions] = useState([]);
-    //
-    // const ulRef = useRef();
-    // const inputRef = useRef();
-    //
-    // useEffect(() => {
-    //     inputRef.current.addEventListener('click', (event) => {
-    //         event.stopPropagation();
-    //         ulRef.current.style.display = 'flex';
-    //         onSearchHandler(event);
-    //     })
-    //     document.addEventListener('click', (event) => {
-    //         ulRef.current.style.display = 'none';
-    //     });
-    // },[]);
-    //
-    // const onSearchHandler = (event) => {
-    //     setOptions(defaultOptions.filter(option => option.includes(event.target.value)));
-    // }
-
-
+function EditRemoveFootage() {
 
     const [allImageNameArr, setAllImageNamesArr] = useState([]);
 
@@ -62,37 +33,35 @@ function EditRemoveFootage(props) {
     }
 
 
-    return (
-        // <div className="search-bar-dropdown">
-        //     <input id="search-bar" type="text" ref={inputRef} className="form-control" placeholder="Search" onChange={onSearchHandler}/>
-        //     <ul id="results" className="list-group" ref={ulRef}>
-        //         {
-        //             options.map((option, index) => (
-        //                 <button key={index} type="button" className="list-group-item list-group-item-dark" onClick={(e) =>{
-        //                     inputRef.current.value = option;
-        //                 }}>
-        //                     {option}
-        //                 </button>
-        //             ))
-        //         }
-        //     </ul>
-        //     <button className="btn btn-primary">Search</button>
-        // </div>
+    const searchImageHandler = (e) => {
+        alert(inputRef.current.value + "_" + localStorage.getItem("uniqueIdPart"));
+    }
 
+    return (
         <div className="search-bar-dropdown">
-            <input id="search-bar" type="text" ref={inputRef} className="form-control" placeholder="Search..." onChange={onSearchHandler}/>
-            <ul id="results" className="list-group" ref={buttonRef}>
-                {
-                    allImageNameArr.map((imageNames, index) => (
-                        <button key={index} type="button" className="list-group-item list-group-item-dark text-end" onClick={(event => {
-                            inputRef.current.value = imageNames.footage_name;
-                        })}>
-                            {imageNames.footage_name}
-                        </button>
-                    ))
-                }
-            </ul>
-            <button className="btn btn-primary">Search</button>
+            <div className="search-bar">
+                <div className="row">
+                    <div className="col-10">
+                        <input id="search-bar" type="text" ref={inputRef} className="form-control" placeholder="Search..." onChange={onSearchHandler}/>
+                        <ul id="results" className="list-group" ref={buttonRef}>
+                            {
+                                allImageNameArr.map((imageNames, index) => (
+                                    <button key={index} type="button" className="list-group-item list-group-item-dark text-end" onClick={(event => {
+                                        inputRef.current.value = (imageNames.footage_name).split('_')[0];
+                                        localStorage.setItem("uniqueIdPart", (imageNames.footage_name).split('_')[1]);
+                                    })}>
+                                        {(imageNames.footage_name).split('_')[0]}
+                                    </button>
+                                ))
+                            }
+                        </ul>
+                    </div>
+                    <div className="col-2">
+                        <button className="btn search-button" onClick={searchImageHandler}>Search</button>
+                    </div>
+                </div>
+            </div>
+
         </div>
     );
 }
